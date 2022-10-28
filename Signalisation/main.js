6c6e560132d0076b4fec369cd899cf02f6e691f7
Ashvin Panchoo <ashvinpanchoo@live.fr>
2022-10-28 10:59:51 +0400

    signalisation example

--- Signalisation/main.js
+++ Signalisation/main.js
@@ -1,1 +1,44 @@
-console.log("test");
+var WebSocketServer = require('ws').Server;
+
+var socket = new WebSocketServer({port : 9090});
+
+var utilisateurs = {};
+
+socket.on('connection', (connection)=>{
+    console.log("l'utilisateur est connecté");
+
+    connection.on('message', ()=>{
+        var data;
+        // verifie si c'est du json
+        try{
+            data = JSON.parse(message);
+        } catch (e) {
+            console.log("Ne supporte que des JSON");
+            data = {}
+        }
+
+        if (data.type == "login"){
+            console.log("Utilisateur en ligne:", data.name);
+            
+            if(utilisateurs[data.name]){
+                sendTo(connection, {
+                    type: "login",
+                    success: false
+                });
+            } else {
+                utilisateurs[data.name] = connection;
+                connection.name = data.name;
+
+                sendTo(connection,{
+                    type: "login",
+                    success: true
+                });
+            }
+
+            
+
+        }
+    });
+
+    connection.send("Hello from server");
+});
